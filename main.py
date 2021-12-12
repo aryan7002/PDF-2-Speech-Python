db15d6e5bf8d3d0ac31deec9a265159e39a6c4ab
aryan <aryandangar7@gmail.com>
2021-12-12 14:21:03 +0530

    work done

---
+++ main.py
@@ -0,0 +1,12 @@
+import pyttsx3 #pip install pyttsx3
+import PyPDF2  #pip install pypdf2
+book =open('sample.pdf','rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages= pdfReader.numPages
+print(pages)
+speak=pyttsx3.init()
+for num in range(1,pages):
+    page =pdfReader.getPage(1)
+    text =page.extractText()
+    speak.say(text)
+    speak.runAndWait()
